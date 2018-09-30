1a143467da3b171a18b467e12fd94eb7ac9e4056
Tom <ganleyapps@gmail.com>
2018-09-29 17:16:57 -0700

    Add a skeleton error type for the app.

---
+++ FlickrSearch/Common/FlickrSearchError.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrSearchError.swift
+//  FlickrSearch
+//
+//  Created by Thomas Ganley on 9/29/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import Foundation
+
+enum FlickrSearchError: Error {
+    
+}
