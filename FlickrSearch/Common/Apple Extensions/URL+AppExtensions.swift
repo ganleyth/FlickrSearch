27a620af0bdde5c209d46ef0d00654eaac8f7c00
Tom <ganleyapps@gmail.com>
2018-09-29 18:48:27 -0700

    Implement network manager request method and an extension on URL for adding query parameters.

---
+++ FlickrSearch/Common/Apple Extensions/URL+AppExtensions.swift
@@ -0,0 +1,27 @@
+//
+//  URL+AppExtensions.swift
+//  FlickrSearch
+//
+//  Created by Thomas Ganley on 9/29/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    /**
+     Generates a URL that results from adding the provided query parameters.
+     
+     - parameters:
+     - queryParameters: The parameters to be added to the URL in a dictionary format
+     
+     - returns: The original URL with provided query parameters added
+     */
+    func adding(queryParameters: [String: String]) -> URL {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: false)
+        let queryItems = queryParameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        components?.queryItems = queryItems
+        
+        return components?.url ?? self
+    }
+}
--- FlickrSearch/Common/Managers/NetworkManager.swift
+++ FlickrSearch/Common/Managers/NetworkManager.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+protocol URLSessionTestable {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionTestable {}
+
+class NetworkManager {
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+        case delete = "DELETE"
+        case put = "PUT"
+    }
+    
+    static var urlSession: URLSessionTestable = URLSession.shared
+    
+    static func performRequestFor(url: URL,
+                                  queryParameters: [String: String]? = nil,
+                                  httpMethod: HTTPMethod = .get,
+                                  completion: @escaping (_ data: Data?, _ response: URLResponse?, _ error: Error?) -> Void) {
+        var requestURL = url
+        if let queryParameters = queryParameters {
+            requestURL = url.adding(queryParameters: queryParameters)
+        }
+        
+        let request = URLRequest(url: requestURL)
+        let dataTask = NetworkManager.urlSession.dataTask(with: request) { (data, response, error) in
+            DispatchQueue.main.async {
+                completion(data, response, error)
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
