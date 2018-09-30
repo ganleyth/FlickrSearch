cf6e5bfa2e635e4458d794c290bc11b06c63211f
Tom <ganleyapps@gmail.com>
2018-09-30 09:33:27 -0700

    Add a background view to the search view table view and add a default text instructing the user to enter a search term upon load.

---
+++ FlickrSearch/Common/Apple Extensions/UIColor+AppExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+AppExtensions.swift
+//  FlickrSearch
+//
+//  Created by Thomas Ganley on 9/30/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var appDarkGrey: UIColor = UIColor(red: 40/255.0, green: 40/255.0, blue: 40/255.0, alpha: 1)
+    static var appDarkerGrey: UIColor = UIColor(red: 29/255.0, green: 29/255.0, blue: 29/255.0, alpha: 1)
+}
--- FlickrSearch/Experiences/PhotoSearchView/PhotoSearchViewController.swift
+++ FlickrSearch/Experiences/PhotoSearchView/PhotoSearchViewController.swift
@@ -13,15 +13,48 @@ final class PhotoSearchViewController: UIViewController {
     @IBOutlet private(set) var interactor: PhotoSearchViewInteractor!
     @IBOutlet private weak var tableView: UITableView! {
         didSet {
-            tableView.register(PhotoTableViewCell.self, forCellReuseIdentifier: "photoCell")
+            setupTableView()
         }
     }
     @IBOutlet private weak var searchBar: UISearchBar!
+    
+    private lazy var enterSearchTermLabel: UILabel? = {
+        let label = UILabel(frame: .zero)
+        label.numberOfLines = 0
+        label.text = NSLocalizedString("ENTER_SEARCH_TERM", comment: "")
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24.0, weight: .bold)
+        label.alpha = 0.3
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = interactor
         searchBar.delegate = interactor
+    }
+}
+
+// MARK: - Private helpers
+private extension PhotoSearchViewController {
+    func setupTableView() {
+        tableView.register(PhotoTableViewCell.self, forCellReuseIdentifier: "photoCell")
+        let backgroundView = UIView(frame: .zero)
+        backgroundView.backgroundColor = .appDarkGrey
+        tableView.backgroundView = backgroundView
+        if let label = enterSearchTermLabel {
+            enhanceTableViewBackground(with: label)
+        }
+    }
+    
+    func enhanceTableViewBackground(with label: UILabel) {
+        guard let backgroundView = tableView.backgroundView else { return }
+        backgroundView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor).isActive = true
+        label.widthAnchor.constraint(equalTo: backgroundView.widthAnchor, multiplier: 0.8).isActive = true
+        label.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 100).isActive = true
     }
 }
